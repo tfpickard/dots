650974edce78d4ea9afdae9406a35b08c41d9694
Tom Pickard <tom.pickard@kelvininc.com>
2019-06-20 16:05:08 -0600

    not happy yet

--- misc/bin/weather.sh
+++ misc/bin/weather.sh
@@ -1,78 +1,55 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# Based on http://openweathermap.org/current
 
-temp_digits="${1:-long}"
+API_KEY="2a899fcbd064d92dee6293115b08bf7a"
 
-# to source user location as $airport var
-. ~/.config/userlocation
+# Check on http://openweathermap.org/find
+CITY_ID="${BLOCK_INSTANCE}"
 
-report="$(weather -q "$airport")"
+URGENT_LOWER=0
+URGENT_HIGHER=30
 
-temperature="$(echo "$report" | grep -i temp | cut -d " " -f2-)"
-temperature="${temperature/ F/°F}"
-temperature="${temperature/ C/°C}"
+ICON_SUNNY=""
+ICON_CLOUDY=""
+ICON_RAINY=""
+ICON_STORM=""
+ICON_SNOW=""
+ICON_FOG=""
 
-if [ "$temp_digits" = "short" ]; then
-	temperature="${temperature/.*°F/°F}"
-	temperature="${temperature/.*°C/°C}"
+SYMBOL_CELSIUS="℃"
+
+WEATHER_URL="http://api.openweathermap.org/data/2.5/weather?id=${CITY_ID}&appid=${API_KEY}&units=metric"
+
+WEATHER_INFO=$(wget -qO- "${WEATHER_URL}")
+WEATHER_MAIN=$(echo "${WEATHER_INFO}" | grep -o -e '\"main\":\"[a-Z]*\"' | awk -F ':' '{print $2}' | tr -d '"')
+WEATHER_TEMP=$(echo "${WEATHER_INFO}" | grep -o -e '\"temp\":\-\?[0-9]*' | awk -F ':' '{print $2}' | tr -d '"')
+
+if [[ "${WEATHER_MAIN}" = *Snow* ]]; then
+  echo "${ICON_SNOW} ${WEATHER_TEMP}${SYMBOL_CELSIUS}"
+  echo "${ICON_SNOW} ${WEATHER_TEMP}${SYMBOL_CELSIUS}"
+  echo ""
+elif [[ "${WEATHER_MAIN}" = *Rain* ]] || [[ "${WEATHER_MAIN}" = *Drizzle* ]]; then
+  echo "${ICON_RAINY} ${WEATHER_TEMP}${SYMBOL_CELSIUS}"
+  echo "${ICON_RAINY} ${WEATHER_TEMP}${SYMBOL_CELSIUS}"
+  echo ""
+elif [[ "${WEATHER_MAIN}" = *Cloud* ]]; then
+  echo "${ICON_CLOUDY} ${WEATHER_TEMP}${SYMBOL_CELSIUS}"
+  echo "${ICON_CLOUDY} ${WEATHER_TEMP}${SYMBOL_CELSIUS}"
+  echo ""
+elif [[ "${WEATHER_MAIN}" = *Clear* ]]; then
+  echo "${ICON_SUNNY} ${WEATHER_TEMP}${SYMBOL_CELSIUS}"
+  echo "${ICON_SUNNY} ${WEATHER_TEMP}${SYMBOL_CELSIUS}"
+  echo ""
+elif [[ "${WEATHER_MAIN}" = *Fog* ]] || [[ "${WEATHER_MAIN}" = *Mist* ]]; then
+  echo "${ICON_FOG} ${WEATHER_TEMP}${SYMBOL_CELSIUS}"
+  echo "${ICON_FOG} ${WEATHER_TEMP}${SYMBOL_CELSIUS}"
+  echo ""
+else
+  echo "${WEATHER_MAIN} ${WEATHER_TEMP}${SYMBOL_CELSIUS}"
+  echo "${WEATHER_MAIN} ${WEATHER_TEMP}${SYMBOL_CELSIUS}"
+  echo ""
 fi
 
-temperature_F="${temperature% (*}"
-
-temperature_C="${temperature#*(}"
-temperature_C="${temperature_C%)*}"
-
-time="$(date +%H)"
-weather="$(echo "$report" | grep -i "Weather:" | cut -d " " -f3-)"
-sky_conditions="$(echo "$report" | grep -i sky | cut -d " " -f3-)"
-
-if [ "$time" -gt "06" ] && [ "$time" -lt "18" ]; then #it's daytime
-	
-	if [ ! "$weather" = "" ]; then #weather is occuring
-		case "$weather" in
-			"light rain"|"rain")
-				weather_icon=""
-				;;
-			*)
-				weather_icon=""
-				;;
-		esac
-
-	else # no weather is occuring; use sky conditions instead
-		case "$sky_conditions" in
-			"overcast"|"mostly cloudy")
-				weather_icon=""
-				;;
-			"clear")
-				weather_icon=""
-				;;
-			*)
-				weather_icon=""
-				;;
-		esac
-	fi
-
-else #it's nighttime
-	if [ ! "$weather" = "" ]; then #weather is occuring
-		case "$weather" in
-			"light rain"|"rain")
-				weather_icon=""
-				;;
-		esac
-
-	else # no weather is occuring; use sky conditions instead
-		case "$sky_conditions" in
-			"overcast"|"mostly cloudy")
-				weather_icon=""
-				;;
-			"clear")
-				weather_icon=""
-				;;
-			*)
-				weather_icon=""
-				;;
-		esac
-	fi
-
+if [[ "${WEATHER_TEMP}" -lt "${URGENT_LOWER}" ]] || [[ "${WEATHER_TEMP}" -gt "${URGENT_HIGHER}" ]]; then
+  exit 33
 fi
-	
-echo "$weather_icon $temperature_F"
